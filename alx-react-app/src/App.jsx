1a72969b54243048575fb615122a18d05b5c18d8
Isa8205 <kibetdavis848@gmmail.com>
2025-07-16 14:15:49 +0300

    Add Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,29 +1,16 @@
-import { useState } from 'react';
-import './App.css';
-import WelcomeMessage from './components/WelcomeMessage';
+import './App.css'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      {/* ✅ New component */}
-      <WelcomeMessage />
-
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <MainContent />
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App

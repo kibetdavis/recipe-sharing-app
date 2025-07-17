0585ae2229fd184d0e0d40495d7ac1fd56f70d34
Isa8205 <kibetdavis848@gmmail.com>
2025-07-17 09:37:59 +0300

    Add UserProfile component and integrate it in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,16 +1,31 @@
-import './App.css'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
+import { useState } from 'react';
+import './App.css';
+import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <Header />
+      <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <MainContent />
       <Footer />
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
